b3d96d53998cc6a8a70664c6154a1b8a82cc5a26
MoneyIgos <makowskiigor@gmail.com>
2020-04-09 17:47:13 +0200

    Add initial files

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const{ prefix, token } = require('./config.json');
+
+bot.on('ready', () => 
+{
+    console.log('Bot is online');
+    bot.user.setActivity('>help');
+})
+bot.login(token);
+
+bot.on('msg', msg => {
+    
+})
