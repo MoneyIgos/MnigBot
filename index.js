d00e32128a3d99d3d5ecbafef55637f3551817ee
MoneyIgos <makowskiigor@gmail.com>
2020-04-09 18:16:40 +0200

    xD

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const{ prefix, token } = require('./config.json');
+const s = '';
 
 bot.on('ready', () => 
 {
     console.log('Bot is online');
     bot.user.setActivity('>help');
 })
-bot.login(token);
+bot.login(token);ssdadasdw
 
 bot.on('msg', msg => {
-    
+
 })
