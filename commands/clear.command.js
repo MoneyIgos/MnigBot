d35a930e4f8973e86c66af80183352e80a10828b
MoneyIgos <makowskiigor@gmail.com>
2020-07-22 12:50:30 +0200

    Add clearing member

--- commands/clear.command.js
+++ commands/clear.command.js
@@ -4,6 +4,7 @@ module.exports = {
 
   run(message, args) {
     const amount = args.join(' ');
+    const member = message.mentions.members.first();
 
     if (!message.member.hasPermission(['MANAGE_MESSAGES']))
       return message.reply('Ughh... You need Manage Messages permissions to perform this command!');
@@ -11,7 +12,7 @@ module.exports = {
     if (!message.guild.me.hasPermission(['MANAGE_MESSAGES']))
       return message.reply("I don't have permission to perform this command!");
 
-    if (isNaN(amount) || !amount)
+    if (isNaN(amount) || !amount || !member)
       return message.reply('You have to specify an amount of messages to delete.');
 
     if (amount > 100) return message.reply("You can't clear more than 100 messages at once");
